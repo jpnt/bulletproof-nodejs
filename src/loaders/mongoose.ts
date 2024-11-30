8f97b5771dafa13d0e629354a69ec037d4189ca9
João Pinto <joaopintojpgp@gmail.com>
2024-11-30 17:53:30 +0000

    update packages and mongoose db connection err

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,6 +3,11 @@ import { Db } from 'mongodb';
 import config from '../../config';
 
 export default async (): Promise<Db> => {
-  const connection = await mongoose.connect(config.databaseURL);
-  return connection.connection.db;
+  try {
+    mongoose.set('strictQuery', true);
+    const connection = await mongoose.connect(config.databaseURL);
+    return connection.connection.db;
+  } catch (err) {
+    throw new Error(`Database connection error: ${err}`)
+  }
 };
